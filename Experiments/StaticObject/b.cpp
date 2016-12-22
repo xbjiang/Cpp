51ada894235e4192b4ef5dc1255022714f5f5ecb
xbjiang <jxavierphysics@163.com>
2016-12-22 11:18:50 +0800

    minor changes

--- Experiments/StaticObject/b.cpp
+++ Experiments/StaticObject/b.cpp
@@ -9,11 +9,11 @@ extern int a; // global variable has to be declared with extern again here
 class A
 {
 public:
-	const static int a = 1; // why this has to be const?
+	const static int a = 1; // why this has to be const? in-class initialization for static members must be static const in.
 	static int b;
+    int c = 1;
 };
-int A::b = 2; // when this variable is initialized?
-
+int A::b = 2; // when this variable is initialized? first time you use it.
 
 int main()
 {
@@ -24,4 +24,6 @@ int main()
 	test();
 	cout << "A::a " << A::a << endl;
 	cout << "A::b " << A::b << endl;
+   
 }
+
