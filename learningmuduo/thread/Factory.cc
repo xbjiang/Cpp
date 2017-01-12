b0525e5eff3bb5fccc8d4868ca15358229471424
xbjiang <jxavierphysics@163.com>
2017-01-12 16:45:46 +0800

    weak call back

--- learningmuduo/thread/Factory.cc
+++ learningmuduo/thread/Factory.cc
@@ -145,6 +145,56 @@ class StockFactory : public boost::enable_shared_from_this<StockFactory>,
     }
 };
 }
+
+class StockFactory : boost::noncopyable, 
+                     public boost::enable_shared_from_this<StockFactory>
+{
+  public:
+    boost::shared_ptr<Stock> get(const string& key)
+    {
+      boost::shared_ptr<Stock> pStock;
+      muduo::MutexLockGuard lock(mutex_);
+      boost::weak_ptr<Stock>& wkStock = stocks_[key];
+      pStock = wkStock.lock();
+      if (pStock)
+      {
+        pStock.reset(new Stock(key), boost::bind(&StockFactory::weakDeleteCallback, 
+                                                 boost::weak_ptr<StockFactory>(shared_from_this()),
+                                                 _1));
+        wkStock = pStock;
+      }
+      return pStock;
+    }
+  
+  private:
+    mutable muduo::MutexLock mutex_;
+    std::map< string, boost::weak_ptr<Stock> > stocks_;
+    
+    static void weakDeleteCallback(const boost::weak_ptr<StockFactory>& wkFactory, 
+                                   Stock* stock)
+    {
+      printf("deleteStock[%p]", stock);
+      boost::shared_ptr<StockFactory> factory(wkFactory.lock());
+      if (factory)
+      {
+        factory->removeStock(stock);
+      }
+      else
+      {
+        printf("factory is dead.\n");
+      }
+      delete stock;
+    }
+    
+    void removeStock(Stock* stock)
+    {
+      if (stock)
+      {
+        muduo::MutexLockGuard lock(mutex_);
+        stocks_.erase(stock->key());
+      }
+    }
+};
 int main()
 {
  /* version1::StockFactory sf1;
@@ -168,5 +218,10 @@ int main()
   boost::shared_ptr<version4::StockFactory> sf4(new version4::StockFactory);
   {
     boost::shared_ptr<Stock> s4 = sf4->get("Stock4");
+  }
+  
+  boost::shared_ptr<StockFactory> sf5(new StockFactory);
+  {
+    boost::shared_ptr<Stock> s5 = sf5->get("Stock5");
   }
 }
